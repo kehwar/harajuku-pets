b470416a26bca69b9926caaeaa8bfe1dd832cc44
kehwar <erickkwr@gmail.com>
2022-04-04 16:09:27 -0500

    Add stock text to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-    <v-row justify="center" align="center"> </v-row>
+    <div class="container">
+        <HarajukuLogo class="logo"></HarajukuLogo>
+        <p>¡Vive Miau!</p>
+        <p>¡Siente Guau!</p>
+    </div>
 </template>
 
 <script>
+import HarajukuLogo from '~/components/harajukuLogo.vue'
 export default {
     name: 'IndexPage',
+    components: { HarajukuLogo },
 }
 </script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+}
+
+.logo {
+    height: 100px;
+}
+
+p {
+    text-align: center;
+}
+</style>
